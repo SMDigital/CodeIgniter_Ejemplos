2369e41e41b591e4499f1b33b3620acd9562b45c
unknown <dalondono@DALONDONO.smdigital.local>
2012-08-21 14:43:41 -0500

    Recuerden de cambiar los parametros de los siguientes archivos:
    .htaccess
    config.php

--- ExampleI/application/config/config.php
+++ ExampleI/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/SitioPruebas/siteSMAcademy_CI/';
+$config['base_url']	= 'http://localhost:8080/SitioPruebas/siteSMAcademy_CI/ExampleI/';
 
 /*
 |--------------------------------------------------------------------------
